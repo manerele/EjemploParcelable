32ceec480d48815270621d455128bb4aaabcadd6
Manuel <manerele@telefonica.net>
2018-09-30 21:48:11 +0100

    Modificar DetailActivity

---
+++ app/src/main/java/com/example/josemanuelgarciacruz/ejemploparcelable/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.josemanuelgarciacruz.ejemploparcelable;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextInputEditText textInputEditTextNombre;
+    TextInputEditText textInputEditTextEdad;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        textInputEditTextNombre = findViewById(R.id.textImpimputEditTextName);
+        textInputEditTextEdad = findViewById(R.id.textImpimputEditTextEdad);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate();
+            }
+        });
+    }
+
+    private void validate() {
+        boolean error = false;
+
+        textInputEditTextNombre.setError(null);
+        textInputEditTextEdad.setError(null);
+
+
+        String name = textInputEditTextNombre.getText().toString();
+
+        if (TextUtils.isEmpty(name)){
+            textInputEditTextNombre.setError(getString(R.string.campo_requerido));
+            textInputEditTextNombre.requestFocus();
+            error = true;
+        }
+
+        if(!error){
+            sendInfoToDetailActivity();
+        }
+
+    }
+
+    private void sendInfoToDetailActivity() {
+        Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
